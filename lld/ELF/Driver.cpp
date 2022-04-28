622df73c3366db3e0cc6c4de2eb9db9c44ebd516
Pavlos Petoumenos <ppetoumenos@gmail.com>
2022-04-28 15:18:32 +0000

    fix

--- lld/ELF/Driver.cpp
+++ lld/ELF/Driver.cpp
@@ -2093,11 +2093,7 @@ static void redirectSymbols(ArrayRef<WrappedSymbol> wrapped) {
               : sym1->section == sym2->section && sym1->value == sym2->value) {
         // Due to an assembler design flaw, if foo is defined, .symver foo,
         // foo@v1 defines both foo and foo@v1. Unless foo is bound to a
-<<<<<<< HEAD
-        // different version, GNU ld makes foo@v1 canonical and eliminates foo.
-=======
         // different version, GNU ld makes foo@v1 canonical and elimiates foo.
->>>>>>> upstream/release/13.x
         // Emulate its behavior, otherwise we would have foo or foo@@v1 beside
         // foo@v1. foo@v1 and foo combining does not apply if they are not
         // defined in the same place.
